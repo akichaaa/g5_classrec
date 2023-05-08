5859166da4658576f31f1c152ecd127160e585dc
akichaaa <124745277+akichaaa@users.noreply.github.com>
2023-05-08 11:00:16 +0800

    Update config.php

--- resource/php/class/config.php
+++ resource/php/class/config.php
@@ -1,13 +1,13 @@
 <?php
 
 class config{
-    private $user = 'root';
-    private $password = '';
+    private $user = 'bsit3567_group5';
+    private $password = 'group5';
     public $pdo = null;
 
     public function con(){
         try {
-            $this->pdo = new PDO('mysql:host=127.0.0.1:3307;dbname=g5_classrec', $this->user, $this->password);
+            $this->pdo = new PDO('mysql:local=109.106.254.194:3307;dbname=bsit3567_g5_classrec', $this->user, $this->password);
             } catch (PDOException $e) {
                 die($e->getMessage());
         }
